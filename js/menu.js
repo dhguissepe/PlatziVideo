9e8f3b6f0c32e41e247ec639357065b149dd4c38
Daniel H. Guissepe <ing.dhguissepe@gmail.com>
2019-07-14 04:50:27 -0500

    Actualización de menú desplegable de la versión móvil, se ha retomado el look anterior, y se ha mejorado la transición para que funcione sin bugs.

--- js/menu.js
+++ js/menu.js
@@ -21,33 +21,29 @@ function isMobile(question)
     if (question)
     {
         arrowButtom.addEventListener("click", active);
-        cross.addEventListener("click", active);
     }
     else
     {
         arrowButtom.removeEventListener("click", active);
         cross.removeEventListener("click", active);
         menu.classList.remove("isactive");
-        topMenu.classList.remove("isactive");
-        cross.classList.remove("isactive");
     }
 }
 
 function active()
 {
-    var attribute = menu.classList.contains("isactive");
-    
-    if (attribute)
+    var isactive = menu.classList.contains("isactive");
+
+    if (isactive)
     {
         menu.classList.remove("isactive");
-        topMenu.classList.remove("isactive");
-        cross.classList.remove("isactive");
+        arrowButtom.classList.remove("icon-circle-down");
+        arrowButtom.classList.add("icon-circle-up");
     }
     else
     {
         menu.classList.add("isactive");
-        topMenu.classList.add("isactive");
-        cross.classList.add("isactive");
+        arrowButtom.classList.remove("icon-circle-up");
+        arrowButtom.classList.add("icon-circle-down");
     }
-    
 }
